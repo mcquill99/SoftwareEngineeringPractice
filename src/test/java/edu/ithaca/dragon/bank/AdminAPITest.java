248c84318598f771c1974a6ca3bdc44b927686f4
Matthew Becker <mbecker5@ithaca.edu>
2020-02-06 21:47:25 -0500

    Added basic tests for freeze and unfreeze

--- src/test/java/edu/ithaca/dragon/bank/AdminAPITest.java
+++ src/test/java/edu/ithaca/dragon/bank/AdminAPITest.java
@@ -6,8 +6,55 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AdminAPITest {
 
 
+    @Test
+    void freezeTest(){
+        CentralBank bankAccount = new CentralBank();
+        CentralBank bankAccount2 = new CentralBank();
+        CentralBank bankAccount3 = new CentralBank();
+        bankAccount.createAccount("11212", "a@b.com", "testingPassword", 500);
+        bankAccount2.createAccount("11BFWGG", "tester@gmail.com", "singleLetter", 1000);
+        bankAccount3.createAccount("test123", "a@b.com", "testpass", 1000);
 
+        AssertEquals(false, bankAccount.getIsFrozen);
+        AssertEquals(false, bankAccount2.getIsFrozen);
+        AssertEquals(false, bankAccount3.getIsFrozen);
 
+        bankAccount.freeze();
+        bankAccount2.freeze();
+        bankAccount3.freeze();
+
+        AssertEquals(true, bankAccount.getIsFrozen);
+        AssertEquals(true, bankAccount2.getIsFrozen);
+        AssertEquals(true, bankAccount3.getIsFrozen);
+
+        //next adding tests to see if when the account is frozen that deposit and other functions do not work
+
+    }
+
+    void unFreezeTest(){
+        CentralBank bankAccount = new CentralBank();
+        CentralBank bankAccount2 = new CentralBank();
+        CentralBank bankAccount3 = new CentralBank();
+        bankAccount.createAccount("11212", "a@b.com", "testingPassword", 500);
+        bankAccount2.createAccount("11BFWGG", "tester@gmail.com", "singleLetter", 1000);
+        bankAccount3.createAccount("test123", "a@b.com", "testpass", 1000);
+
+        bankAccount.freeze();
+        bankAccount2.freeze();
+        bankAccount3.freeze();
+
+        AssertEquals(true, bankAccount.getIsFrozen);
+        AssertEquals(true, bankAccount2.getIsFrozen);
+        AssertEquals(true, bankAccount3.getIsFrozen);
+
+        bankAccount.unreeze();
+        bankAccount2.unfreeze();
+        bankAccount3.unfreeze();
+
+        AssertEquals(false, bankAccount.getIsFrozen);
+        AssertEquals(false, bankAccount2.getIsFrozen);
+        AssertEquals(false, bankAccount3.getIsFrozen);
+    }
 
 
 }
